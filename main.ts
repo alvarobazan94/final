1af527bdf914ec5e1be1dae217ceb0a60ec1a26c
alvarobazan94 <20130147@aloe.ulima.edu.pe>
2019-07-18 23:58:36 -0500

    primer_commit

---
+++ main.ts
@@ -0,0 +1,40 @@
+import express =  require("express");
+
+export class Calculadora
+{
+    sumar(op1:number, op2 : number)
+    {
+        return op1 + op2
+    }
+}
+
+let main = () =>
+{
+    let app : express.Application = express()
+
+    app.set('view engine', 'pug')
+    app.set('views', './views')
+
+    app.get('/calculadora', function(req: any, res : any){
+        res.render('calculadora', {
+            suma : "0"
+        })
+    })
+    app.get('/sumar', function(req: any, res : any){
+        let op1 : string = req.query.op1
+        let op2 : string = req.query.op2
+        let calculadora = new Calculadora()
+        res.render('calculadora', {
+            suma : calculadora.sumar(parseInt(op1), parseInt(op2))
+        })
+    })
+
+    // Iniciamos la ejecucion del server
+    let puerto = process.env.PORT || 3000
+
+    app.listen(puerto, ()=>{
+        console.log(`Server running in port ${puerto}`);
+    })
+}
+
+main()
